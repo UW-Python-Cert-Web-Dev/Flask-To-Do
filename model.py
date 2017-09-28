a7dd4f3da36b89f53d8135344b4602ad48b55b14
Joseph Schilz <joseph@schilz.org>
2017-09-27 21:49:00 -0700

    Initial commit.

---
+++ model.py
@@ -0,0 +1,35 @@
+from peewee import Model, CharField, DateTimeField, ForeignKeyField
+from playhouse.sqlite_ext import SqliteExtDatabase
+
+db = SqliteExtDatabase('my_database.db')
+
+
+class User(Model):
+    name = CharField(max_length=255, unique=True)
+    password = CharField(max_length=255)
+
+    class Meta:
+        database = db
+
+
+class Task(Model):
+    name = CharField(max_length=255)
+    performed = DateTimeField(null=True)
+    performed_by = ForeignKeyField(rel_model=User, null=True)
+
+    class Meta:
+        database = db
+
+
+# class Context(BaseModel):
+#     name = CharField(max_length=255)
+#
+#
+# class ContextTask(BaseModel):
+#     context_id = IntegerField()  # foreign key into context
+#     task_id = IntegerField()  # foreign key into task
+#
+#     class Meta:
+#         primary_key = CompositeKey(
+#             'context_id', 'task_id'
+#         )
---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Scripts to run to set up our database
+"""
+
+from model import db, User, Task
+from passlib.hash import pbkdf2_sha256
+
+
+# Create the database tables for our model
+db.connect()
+db.create_tables([User, Task])
+
+# We need a user to log in as! Create it here.
+first_user_name = "admin"
+first_user_password = "password"
+
+first_user = User(name=first_user_name, password=pbkdf2_sha256.hash(first_user_password))
+first_user.save()
